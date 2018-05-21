db519067d551fcbc7aee3c9c54437d203d8c6e2f
Nadia Irvin <nadia.irvin@lessannoyingcrm.com>
2018-05-21 16:53:48 -0500

    5/21 Final work commit
    I'm trying to figure some different styles to implement.

--- Practice in php/test.php
+++ Practice in php/test.php
@@ -1,8 +1,12 @@
 <?php
 	$name = 'Nadia';
 	$age = 29;
-	echo "Hello, my name is $name and I am $age";
+	echo "Hello, my name is $name and I am $age.<br>";
 	$Variable1 = 5;
 	$Variable2 = 9;
-	function = "$Variable1 + $Variable2 = $sum";
+	function getSum($num1, $num2){
+		$sum = $num1 + $num2;
+		Echo "The sum of the two number $num1 + $num2 is : $sum";
+	}
+	getSum(6425,9)
  ?>
