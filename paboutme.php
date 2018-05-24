3862c7043448bb1bb07401816656e672020eeaa9
Nadia Irvin <nadia.irvin@lessannoyingcrm.com>
2018-05-24 16:55:10 -0500

    5/24 Final at work Commit
    I worked in php some more!

--- paboutme.php
+++ paboutme.php
@@ -16,10 +16,10 @@
 				<img src="/Memphis.jpg" alt="Memphis" style='float: left; display: inline; height:auto; width:25%; margin-left: 100px;'>
 				<p class='youth'> I was born and raised in Memphis, Tn. The town that most outsiders recognize as the home of Elvis, while most residents have never been.</p>
 			</div>
-			<br>
-			<br>
+	<!--		<br>
+			<br>-->
 			<div class="Section2">
-				<img src="/freshman.jpg" alt="FreshmanPhoto" style='float:right; display: inline; height:400px; width:25%; margin-right: 100px; margin-top: 30px;'>
+				<img src="/freshman.jpg" alt="FreshmanPhoto" style='float:right; display: inline; height:400px; width:25%; margin-right: 100px; margin-top: 30px; align: center;'>
 				<p class='education'> I am a rising  Junior at Washington University in St. Louis studying Organization and Strategic Management.</p>
 				<!-- I know these aren't feasibly spaced i will have to look up more effiecient ways
 				-->
