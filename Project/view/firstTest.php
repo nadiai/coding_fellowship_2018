2ad38c7003c6b305d934e3ad3432dfa629731a28
Nadia Irvin <nadia.irvin@lessannoyingcrm.com>
2018-07-18 09:37:28 -0500

    07/18 First Commit
    Accidentelly started something new without creating a new branch

---
+++ Project/view/firstTest.php
@@ -0,0 +1,26 @@
+<?php
+
+include('headerPage.php');
+
+printHeader();
+
+?>
+			<title> Welcome to The Interview </title>
+			<link href='https://fonts.googleapis.com/css?family=Cinzel Decorative' rel='stylesheet'>
+			<style type="text/css">
+			<?php
+			 include('Project/include/projectStyle.css');
+			 ?>
+			</style>
+			<script src="/Blog/include/jquery.js"></script>
+		</head>
+		<body>
+			<div class="firstTestContainer">
+				<div class="firstTestSection1">
+				</div>
+				<div class="firstTest">
+				</div>
+			</div>
+<?php
+printFooter();
+?>
